6bc76fdb82744faf116641330576ff2593424003
Nuran <berdaliyev2005@mail.ru>
2026-06-10 21:24:59 +0500

    models, settings, readme

--- 1-Book-portal/book/book_app/models.py
+++ 1-Book-portal/book/book_app/models.py
@@ -1,3 +1,76 @@
 from django.db import models
+from django.conf import settings
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class Author(models.Model):
+    name=models.CharField(max_length=255)
+    biography=models.TextField(blank=True)
+    def __str__(self):
+        return self.name
+
+class Genre(models.Model):
+    name=models.CharField(max_length=255, unique=True)
+    def __str__(self):
+        return self.name
+    
+class Book(models.Model):
+    added_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='added_books'
+    )
+    name=models.CharField(max_length=255)
+    description=models.TextField(blank=True)
+    published_year=models.PositiveIntegerField(null=True, blank=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    author=models.ForeignKey(
+        Author, 
+        on_delete=models.CASCADE, 
+        related_name='books'
+    )
+    genres=models.ManyToManyField(
+        Genre, 
+        related_name='books',
+        blank=True
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['added_by', 'name', 'author'],
+                name='unique_book_per_addedby_name_author'
+            )
+        ]
+
+    def __str__(self):
+        return self.name
+
+class Review(models.Model):
+    added_by=models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='added_reviews'
+    )
+    book=models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    text=models.TextField()
+    rating=models.PositiveSmallIntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(5),
+        ]
+    )
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['added_by', 'book'],
+                name='unique_review_per_addedby_book'
+            )
+        ]
+    def __str__(self):
+        return f"{self.added_by} - {self.book} - {self.rating}"

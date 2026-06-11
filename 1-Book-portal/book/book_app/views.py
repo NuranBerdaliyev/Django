a0b204c2d589c665709db6dfeca97bf9d9dc3fa3
Nuran <berdaliyev2005@mail.ru>
2026-06-11 22:45:35 +0500

    views

--- 1-Book-portal/book/book_app/views.py
+++ 1-Book-portal/book/book_app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Book, Author, Genre, Review
 
+class BookListView(ListView):
+    model=Book
+    context_object_name='books'
 
+    queryset=Book.objects.select_related(
+        'author', 'added_by'
+    ).prefetch_related('genres')

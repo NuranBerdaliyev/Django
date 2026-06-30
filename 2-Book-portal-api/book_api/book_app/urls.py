65c7904e625b6b83329955be3c57aba3002fbd7c
Nuran <berdaliyev2005@mail.ru>
2026-06-30 11:55:22 +0500

    urls for APIViews

--- 2-Book-portal-api/book_api/book_app/urls.py
+++ 2-Book-portal-api/book_api/book_app/urls.py
@@ -1,4 +1,21 @@
 #book_app/urls.py
 from django.urls import path
+from .views import (
+    AuthorDetailAPIView,
+    AuthorListAPIView,
+    BookDetailAPIView,
+    BookListAPIView,
+    GenreDetailAPIView,
+    GenreListAPIView,
+)
 
-urlpatterns = []
+urlpatterns = [
+    path('authors/', AuthorListAPIView.as_view(), name='api_author_list'),
+    path('authors/<int:pk>/', AuthorDetailAPIView.as_view(), name='api_author_detail'),
+
+    path('genres/', GenreListAPIView.as_view(), name='api_genre_list'),
+    path('genres/<int:pk>/', GenreDetailAPIView.as_view(), name='api_genre_detail'),
+
+    path('books/', BookListAPIView.as_view(), name='api_book_list'),
+    path('books/<int:pk>/', BookDetailAPIView.as_view(), name='api_book_detail'),
+]

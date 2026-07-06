9f94f11f5a78be4488e0ac57c1ac4a7cf0bd79ca
Nuran <berdaliyev2005@mail.ru>
2026-07-06 12:25:19 +0500

    Register API

--- 2-Book-portal-api/book_api/users/views.py
+++ 2-Book-portal-api/book_api/users/views.py
@@ -1,11 +1,10 @@
 #users/views.py
-from rest_framework import generics
+from rest_framework.generics import CreateAPIView 
 from rest_framework.permissions import AllowAny
 
 from .serializers import RegisterSerializer
 
-
-class RegisterAPIView(generics.CreateAPIView):
+class RegisterAPIView(CreateAPIView):
     serializer_class = RegisterSerializer
     permission_classes = [
         AllowAny,
